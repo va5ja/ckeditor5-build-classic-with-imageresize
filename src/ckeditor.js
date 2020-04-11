c73403260fdb11a3399ff801a5d0d14a82214efb
Vasja Laharnar <vasja.laharnar@gmail.com>
2020-04-11 15:08:42 +0200

    Update to CKEditor v18.0.0

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,7 @@ import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
@@ -66,6 +67,7 @@ ClassicEditor.builtinPlugins = [
 	Link,
 	List,
 	MediaEmbed,
+	PageBreak,
 	Paragraph,
 	PasteFromOffice,
 	Table,
@@ -109,6 +111,7 @@ ClassicEditor.defaultConfig = {
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
+			'pageBreak',
 			'undo',
 			'redo'
 		]
